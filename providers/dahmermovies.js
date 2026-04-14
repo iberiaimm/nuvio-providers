1242a77e51e63be6fde29ba7046e933aa22252ce
iberiaimm <iberiaimm@gmail.com>
2026-04-14 11:26:56 +0200

    Update dahmermovies.js

--- providers/dahmermovies.js
+++ providers/dahmermovies.js
@@ -4,7 +4,7 @@
 console.log('[DahmerMovies] Initializing Dahmer Movies scraper');
 
 // Constants
-const TMDB_API_KEY = "439c478a771f35c05022f9feabcca01c";
+const TMDB_API_KEY = "4d8fa4b38dd1070fa957a2d82dcd257c";
 const DAHMER_MOVIES_API = 'https://a.111477.xyz';
 const TIMEOUT = 60000; // 60 seconds
 
